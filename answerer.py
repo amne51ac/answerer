1e57d537d936ea6bad92b7c440875f7364f13aea
amne51ac <th3amne51ac@gmail.com>
2018-04-20 22:30:02 -0500

    for fabo

--- answerer.py
+++ answerer.py
@@ -1,6 +1,7 @@
 import urllib.request
 import urllib.parse
 
-url = 'https://api.spotify.com/v1/search?type=artist&q=snoop'
+url = 'https://www.google.com/search?q={}'
+searchterm = ''
 f = urllib.request.urlopen(url)
 print(f.read().decode('utf-8'))
