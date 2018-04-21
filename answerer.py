e3173cd7b89cd30b836c1cd3b599a3c2d4c75cb6
amne51ac <th3amne51ac@gmail.com>
2018-04-20 22:50:44 -0500

    update

--- answerer.py
+++ answerer.py
@@ -1,7 +1,19 @@
-import urllib.request
+from urllib import request
 import urllib.parse
 
-url = 'https://www.google.com/search?q={}'
-searchterm = ''
-f = urllib.request.urlopen(url)
-print(f.read().decode('utf-8'))
+
+def answerer(question, a1, a2, a3):
+    question = question
+    a1 = a1
+    a2 = a2
+    a3 = a3
+    print('what the fuck')
+
+
+def search(term):
+    url = 'https://www.google.com/search?q={}'
+    f = request.urlopen(url.format(term))
+    return f.read().decode('utf-8')
+
+def split_results(results):
+    pass
